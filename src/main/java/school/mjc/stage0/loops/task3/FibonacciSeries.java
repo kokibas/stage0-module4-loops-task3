6f224a01579f33a32272df39726cb8ba6aeb349a
kokibas <nurym0807@gmail.com>
2022-11-16 17:35:43 +0600

    002

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+        int sum = 0;
+        for (int i = 0; i <= lastFibonacci; ++i ){
+            sum = ( i - 1 ) + ( i - 2);
+        }
+        System.out.println(sum);
     }
 }
