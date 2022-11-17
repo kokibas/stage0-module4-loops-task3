9b0b9959288f26e7feb15688c337c5c2b900222d
kokibas <nurym0807@gmail.com>
2022-11-17 11:08:56 +0600

    003

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.loops.task3;
 
 public class AlphabetPrinter {
     public void printAlphabet() {
-        for (char i = 'A';i <= 'Z';i++){
+        for (char i = 'a';i <= 'z';i++){
             System.out.println(i + " ");
         }
     }
